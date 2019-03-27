f4e0dbdf76aa395035adb0f9ff60d8173c7e8a75
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-03-27 18:41:18 +0100

    [Refactor] Delete commented-out code

--- Services/ArticlesHandler.cs
+++ Services/ArticlesHandler.cs
@@ -39,74 +39,8 @@ namespace Services
 
             articles = stemmedWords;
 
-            /*foreach (var article in articles)
-            {
-                
-                _stopListService = new StopListService(articles);
-
-                List<string> stopListedWords = _stopListService.Call(stopList);
-
-                List<string> stemmedWords = _stemmingService.Call(stopListedWords);
-
-                _termService = new TermFrequencyParserService(stemmedWords);
-
-                Dictionary<string, double> wordFrequency = _termService.Call();
-
-//                AddArticleToIdf(stemmedWords);
-
-                ArticlesDataModels.Add(new ArticleDataModel()
-                {
-                    Article = article,
-                    StopListedWords = stopListedWords,
-                    StemmedWords = stemmedWords,
-                    WordFrequency = wordFrequency,
-                    IdfWordFrequency = new Dictionary<string, double>(wordFrequency)
-                    
-                });
-            }*/
-
-            //_idfService.Call(ref ArticlesDataModels);
-
-            /*GenerateIdfTfFactors();
-            AddTfIdfToArticlesData(ArticlesDataModels);*/
 
         }
-
-/*        public void AddTfIdfToArticlesData(List<ArticleDataModel> Articles)
-        {
-            foreach (var articleDataModel in Articles)
-            {
-                List<string> keys = new List<string>(articleDataModel.IdfWordFrequency.Keys);
-                foreach (var key in keys)
-                {
-                    articleDataModel.IdfWordFrequency[key] *= IdfTfFactorsDictionary[key];
-                }
-                
-            }
-        }*/
-
-        /*public void GenerateIdfTfFactors()
-        {
-            double numOfArticles = ArticlesDataModels.Count;
-            foreach (var i in IdfDictionary)
-            {
-                IdfTfFactorsDictionary.Add(i.Key, Math.Log10(numOfArticles / i.Value));
-            }
-        }*/
-
-        /*public void AddArticleToIdf(List<string> stemmedWords)
-        {
-            List<string> distinctWords = new List<string>(stemmedWords).Distinct().ToList();
-            foreach (var distinctWord in distinctWords)
-            {
-                if (!IdfDictionary.ContainsKey(distinctWord))
-                {
-                    IdfDictionary.Add(distinctWord, 0);
-                }
-
-                IdfDictionary[distinctWord]++;
-            }
-        }*/
 
         
     }
