14b78356f47ff1822f2a491bc40c62792ce58f64
michudzik <michalchudzik@live.com>
2019-04-01 19:41:09 +0200

    [Add] Analyze keyword frequency in first 20% of the text

---
+++ ClassificationServices/Keyword20PercentFrequencyService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassificationServices
+{
+    class Keyword20PercentFrequencyService : IFeatureService
+    {
+        public double Call(List<string> keyWords, List<string> stemmedWords)
+        {
+            int lastIndex = (int)Math.Floor(stemmedWords.Count * 0.2);
+            double keywordFrequency = 0.0;
+            foreach(string keyWord in keyWords)
+            {
+                for (int i = 0; i < lastIndex; i++)
+                {
+                    if (keyWord == stemmedWords[i])
+                    {
+                        keywordFrequency++;
+                    }
+                }
+            }
+
+            return keywordFrequency / lastIndex;
+        }
+    }
+}
