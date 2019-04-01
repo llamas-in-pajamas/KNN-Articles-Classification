0382f6dfec5ab1f04686db89388eac0ab2ef92ca
michudzik <michalchudzik@live.com>
2019-04-01 19:55:26 +0200

    [Add] Binary similarity feature

---
+++ ClassificationServices/BinaryFeatureService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ClassificationServices
+{
+    class BinaryFeatureService : IFeatureService
+    {
+        public double Call(List<string> keyWords, List<string> stemmedWords)
+        {
+            int count = 0;
+            int limit = keyWords.Count;
+            foreach(string keyword in keyWords)
+            {
+                if (stemmedWords.Contains(keyword)) count++;
+            }
+
+            return (count == limit ? 1 : 0);
+        }
+    }
+}
