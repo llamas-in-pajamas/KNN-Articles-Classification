6eaa278edea0f04eb2a379c1457ecb6887510f85
michudzik <michalchudzik@live.com>
2019-04-01 21:22:52 +0200

    [Add] Euclidean distance

---
+++ ClassificationServices/EuclideanDistance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassificationServices
+{
+    class EuclideanDistance : IDistance
+    {
+        public double Call(List<double> v1, List<double> v2)
+        {
+            double result = 0.0;
+            int n = v1.Count;
+            for(int i = 0; i < n; i++)
+            {
+                result += Math.Pow(v1[i] - v2[i], 2);
+            }
+            return Math.Sqrt(result);
+        }
+    }
+}
