de1d96f4c093f57e6a634b7ae65f839da9502372
michudzik <michalchudzik@live.com>
2019-04-01 19:43:58 +0200

    [Add] Analyze keyword count instead of frequency

---
+++ ClassificationServices/KeywordCountFeatureService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ClassificationServices
+{
+    class KeywordCountFeatureService : IFeatureService
+    {
+        public double Call(List<string> keyWords, List<string> stemmedWords)
+        {
+            double keywordCount = 0;
+            foreach(string keyWord in keyWords)
+            {
+                foreach(string word in stemmedWords)
+                {
+                    if (keyWord == word)
+                    {
+                        keywordCount++;
+                    }
+                }
+            }
+            return keywordCount;
+        }
+    }
+}
